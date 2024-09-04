57887340d254a6ef91a0420f84e3dc1bed0056e1
mnyrop <marii@nyu.edu>
2024-09-04 12:26:27 -0400

    comp gh repo too

--- lib/compare_lists.py
+++ lib/compare_lists.py
@@ -6,19 +6,48 @@ with open('data/rw_afiles.txt') as f:
 with open('data/gdrive_afiles.txt') as f:
   gdrive_afiles = [afile.strip() for afile in f]
 
-rw_dupes = [item for item, count in collections.Counter(rw_afiles).items() if count > 1]
-gd_dupes = [item for item, count in collections.Counter(gdrive_afiles).items() if count > 1]
+with open('data/gh_json_afiles.txt') as f:
+  gh_json_afiles = [afile.strip() for afile in f]
+
+with open('data/gh_ocr_afiles.txt') as f:
+  gh_ocr_afiles = [afile.strip() for afile in f]
+
+rw_dupes      = [item for item, count in collections.Counter(rw_afiles).items() if count > 1]
+gd_dupes      = [item for item, count in collections.Counter(gdrive_afiles).items() if count > 1]
+gh_json_dupes = [item for item, count in collections.Counter(gh_json_afiles).items() if count > 1]
+gh_ocr_dupes = [item for item, count in collections.Counter(gh_ocr_afiles).items() if count > 1]
 
 if rw_dupes:
   print(f'RW has afile number dupes: {rw_dupes}\n')
 if gd_dupes:
   print(f'GD has afile number dupes: {gd_dupes}\n')
+if gh_json_dupes:
+  print(f'GH has json afile number dupes: {gh_json_dupes}\n')
+if gh_ocr_dupes:
+  print(f'GH has ocr afile number dupes: {gh_ocr_dupes}\n')
 
-in_rw_not_gd = list(set(rw_afiles) - set(gdrive_afiles))
-in_gd_not_rw = list(set(gdrive_afiles) - set(rw_afiles))
+in_rw_not_gd          = list(set(rw_afiles) - set(gdrive_afiles))
+in_rw_not_gh_json     = list(set(rw_afiles) - set(gh_json_afiles))
+
+in_gd_not_rw          = list(set(gdrive_afiles) - set(rw_afiles))
+in_gd_not_gh_json     = list(set(gdrive_afiles) - set(gh_json_afiles))
+
+in_gh_json_not_rw     = list(set(gh_json_afiles) - set(rw_afiles))
+in_gh_json_not_gd     = list(set(gh_json_afiles) - set(gdrive_afiles))
+
+rw_missing_gd_vs_gh_missing_gd = list(set(in_gd_not_rw) - set(in_gd_not_gh_json))
+
+# in_gh_json_not_gh_ocr = list(set(gh_json_afiles) - set(gh_ocr_afiles))
+# in_gh_ocr_not_gh_json = list(set(gh_ocr_afiles) - set(gh_json_afiles))
 
 with open('data/in_rw_not_gd.txt', 'w+') as file:
   file.write("\n".join(in_rw_not_gd))
 
 with open('data/in_gd_not_rw.txt', 'w+') as file:
   file.write("\n".join(in_gd_not_rw))
+
+with open('data/in_gh_not_rw.txt', 'w+') as file:
+  file.write("\n".join(in_gh_json_not_rw))
+
+with open('data/in_gd_not_gh.txt', 'w+') as file:
+  file.write("\n".join(in_gd_not_gh_json))
---
+++ lib/list_gh_afiles.py
@@ -0,0 +1,37 @@
+import glob
+import os
+
+json_glob_path    = 'extracted-data/data/metadata_outputs/*.json'
+ocr_glob_path     = 'extracted-data/data/text_ocr/*.txt'
+
+json_paths        = glob.glob(json_glob_path)
+ocr_paths         = glob.glob(ocr_glob_path)
+
+json_result_path  = 'data/gh_json_afiles.txt'
+ocr_result_path   = 'data/gh_ocr_afiles.txt'
+
+json_afile_nos  = []
+ocr_afile_nos   = []
+
+for path in json_paths:
+  afile_no = os.path.basename(path)
+  afile_no = afile_no.replace('.json', '')
+  afile_no = afile_no.replace('_redacted', '')
+  afile_no = afile_no.split('_')[0]
+  
+  json_afile_nos.append(afile_no)
+
+
+for path in ocr_paths:
+  afile_no = os.path.basename(path)
+  afile_no = afile_no.replace('.txt', '')
+  afile_no = afile_no.replace('_redacted', '')
+  afile_no = afile_no.split('_')[0]
+  
+  ocr_afile_nos.append(afile_no)
+
+with open(json_result_path, 'w+') as file:
+  file.write("\n".join(list(set(json_afile_nos))))
+
+with open(ocr_result_path, 'w+') as file:
+  file.write("\n".join(list(set(ocr_afile_nos))))
